30dee30b119caa38b7796a1c80eab1c4c20b02ca
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-06-23 18:19:04 +0200

    display the clock on a webpage

--- week1/time/challenge.js
+++ week1/time/challenge.js
@@ -1,9 +1,11 @@
-// 1
+// 1 Time Object Basics
+
 let currentTime = new Date();
 console.log(`Current time: ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`);
 
 
-//2
+// 2 Object-Oriented Clock
+
 // Define a Clock constructor function
 function Clock(hours, minutes, seconds) {
   this.hours = hours;
@@ -17,7 +19,8 @@ let myClock = new Clock(12, 30, 0);
 console.log(`My clock: ${myClock.hours}:${myClock.minutes}:${myClock.seconds}`);
 
 
-// 3
+// 3Time Formatting
+
 // Add methods to the Clock object for formatting time
 function Clock(hours, minutes, seconds) {
   this.hours = hours;
@@ -44,3 +47,15 @@ let ourClock = new Clock(12, 30, 0);
 
 console.log(`Formatted time: ${ourClock.getFormattedTime()}`);
 console.log(`12-hour time: ${ourClock.get12HourTime()}`);
+
+
+//4 function to display the clock on a webpage
+
+function displayClock() {
+    let clockEl = document.getElementById('clock-display');
+    let now = new Date();
+    let clock = new Clock(now.getHours(), now.getMinutes(), now.getSeconds());
+    clockEl.innerHTML = clock.getFormattedTime();
+}
+
+setInterval(displayClock, 1000);
