ab4cf630691052df7da2c567e63bbd2eb4d63d98
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-06-23 17:32:09 +0200

    object oriented clock

--- week1/time/challenge.js
+++ week1/time/challenge.js
@@ -4,3 +4,14 @@ console.log(`Current time: ${currentTime.getHours()}:${currentTime.getMinutes()}
 
 
 //2
+// Define a Clock constructor function
+function Clock(hours, minutes, seconds) {
+  this.hours = hours;
+  this.minutes = minutes;
+  this.seconds = seconds;
+}
+
+// Create a clock instance
+let myClock = new Clock(12, 30, 0);
+
+console.log(`My clock: ${myClock.hours}:${myClock.minutes}:${myClock.seconds}`);
