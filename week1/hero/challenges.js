97c55bbb1e0fbdaecd0ed55bd2ed607a55f2fd4b
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-06-23 20:34:37 +0200

    Add methods to the superhero object

--- week1/hero/challenges.js
+++ week1/hero/challenges.js
@@ -6,3 +6,19 @@ const superhero = {
 };
 
 console.log(superhero);
+
+const superHero = {
+  name: 'The Bible',
+  secretIdentity: 'samson',
+  powers: ['super strength', 'fighting', 'lion killer'],
+  weakness: 'women',
+  usePower: function(powerName) {
+    console.log(`${this.name} is using ${powerName}!`);
+  },
+  revealIdentity: function() {
+    console.log(`The secret identity of ${this.name} is ${this.secretIdentity}!`);
+  }
+};
+
+superHero.usePower('fighting');
+superHero.revealIdentity();
