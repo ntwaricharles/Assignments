4cef23a993812adc8a2a26dfc235b191fdf12ae4
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-06-23 17:47:42 +0200

    time formatting

--- week1/time/challenge.js
+++ week1/time/challenge.js
@@ -15,3 +15,32 @@ function Clock(hours, minutes, seconds) {
 let myClock = new Clock(12, 30, 0);
 
 console.log(`My clock: ${myClock.hours}:${myClock.minutes}:${myClock.seconds}`);
+
+
+// 3
+// Add methods to the Clock object for formatting time
+function Clock(hours, minutes, seconds) {
+  this.hours = hours;
+  this.minutes = minutes;
+  this.seconds = seconds;
+
+  this.getFormattedTime = function() {
+    return `${padZero(this.hours)}:${padZero(this.minutes)}:${padZero(this.seconds)}`;
+  };
+
+  this.get12HourTime = function() {
+    let ampm = this.hours < 12 ? 'AM' : 'PM';
+    let hours12 = this.hours % 12 || 12;
+    return `${padZero(hours12)}:${padZero(this.minutes)}:${padZero(this.seconds)} ${ampm}`;
+  };
+}
+
+// Helper function to pad zeros
+function padZero(num) {
+  return (num < 10 ? '0' : '') + num;
+}
+
+let ourClock = new Clock(12, 30, 0);
+
+console.log(`Formatted time: ${ourClock.getFormattedTime()}`);
+console.log(`12-hour time: ${ourClock.get12HourTime()}`);
