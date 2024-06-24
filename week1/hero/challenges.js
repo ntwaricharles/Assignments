715aa7f1c4b60ac3bfa01f1eb6c0266577c4ccd7
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-06-24 10:28:13 +0200

    Object Iteration and Transformation

--- week1/hero/challenges.js
+++ week1/hero/challenges.js
@@ -54,3 +54,47 @@ const superhero2 = new Superhero('The king', 'David', ['Faith', 'Sniper', 'Hunte
 superhero1.usePower();
 superhero2.revealIdentity();
 
+
+function theSuperHero(name, secretIdentity, powers, weakness) {
+  this.name = name;
+  this.secretIdentity = secretIdentity;
+  this.powers = powers;
+  this.weakness = weakness;
+  this.usePower = function(powerName) {
+    console.log(`${this.name} is using ${powerName}!`);
+  };
+  this.revealIdentity = function() {
+    console.log(`The secret identity of ${this.name} is ${this.secretIdentity}!`);
+  };
+}
+
+theSuperHero.prototype.fly = function() {
+  console.log(`${this.name} is flying!`);
+};
+
+const superhero11 = new theSuperHero('The Bible', 'Samson', ['super strength', 'fight', 'lion killer'], 'women');
+const superhero22 = new theSuperHero('The king', 'David', ['Faith', 'Sniper', 'Hunter'], 'women');
+
+superhero11.fly();
+superhero22.fly();
+
+const superheroes = [
+  { name: 'The Bible', powers: ['super strength', 'fight', 'lion killer'] },
+  { name: 'The king', powers: ['Faith', 'Sniper', 'Hunter'] },
+  { name: 'The Savior', powers: ['Healing', 'milacles', 'salvation'] }
+];
+
+const supervillains = [
+  { name: 'The Ghost', powers: ['thief', 'gossiping'] },
+  { name: 'The Lucifer', powers: ['dark magic', 'lie'] },
+  { name: 'The Sin', powers: ['death', 'hopeless'] }
+];
+
+superheroes.forEach(hero => console.log(hero.name));
+
+const superheroNames = superheroes.map(hero => hero.name);
+console.log(superheroNames);
+
+const darkMagicVillains = supervillains.filter(villain => villain.powers.includes('dark magic'));
+console.log(darkMagicVillains);
+
